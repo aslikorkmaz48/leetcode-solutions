c2e7729125e30afc348778d3ad634d56b27384b4
Aslı Korkmaz(Nes) <korkmaz.asli2003@gmail.com>
2025-12-03 15:04:27 +0300

    Time: 5 ms (58.81%) | Memory: 13.4 MB (37.92%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        // Roma rakamlarını değerleriyle eşleştiriyoruz
+        unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Eğer mevcut karakter bir sonraki karakterden küçükse çıkarılır
+            if (i + 1 < s.length() && values[s[i]] < values[s[i + 1]]) {
+                result -= values[s[i]];
+            } else {
+                result += values[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
